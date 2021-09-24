0fb7f6704f1b1681207e6d1712710408c0325314
PanoramaView <zhou-1997@hotmail.it>
2021-09-24 17:31:32 +0200

    observable.error() and complete()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,13 +22,31 @@ export class HomeComponent implements OnInit {
       let counter = 0;
       setInterval(() => {
         observer.next(counter); //to emit a new value
+
+        if ( counter === 2 ) {
+          observer.complete();
+        }
+
+        if (counter > 3){
+          observer.error(new Error('Count is greater than 3')); //error message in console and the script stops
+        }
         counter ++;
       }, 1000)
     });
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data =>{
       console.log(data);
-    });
+    }, 
+    //handling the error
+    error => {
+      console.log(error);
+      alert(error.message);
+    },
+    //handling complete()
+    () => {
+      console.log('Completed!');
+    }
+    );
 
   }
 
